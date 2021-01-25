5a1845037e8f498436c014cd46dc0961c8555c99
Matheus N <Matheus N>
2021-01-25 17:05:25 -0300

    Adding arrow functions

--- test/eniac.js
+++ test/eniac.js
@@ -1,118 +1,118 @@
 var Eniac = artifacts.require("./Eniac.sol");
 
-contract('Eniac', function (accounts) {
+contract('Eniac', (accounts) => {
   var tokenInstance;
 
-  it('initializes the contract with the correct values', function () {
-    return Eniac.deployed().then(function (instance) {
+  it('initializes the contract with the correct values', () => {
+    return Eniac.deployed().then((instance) => {
       tokenInstance = instance;
       return tokenInstance.name();
-    }).then(function (name) {
+    }).then((name) => {
       assert.equal(name, 'Eniac', 'has the correct name');
       return tokenInstance.symbol();
-    }).then(function (symbol) {
+    }).then((symbol) => {
       assert.equal(symbol, 'ENIAC', 'has the correct symbol');
       return tokenInstance.standard();
-    }).then(function (standard) {
+    }).then((standard) => {
       assert.equal(standard, 'Eniac Token v1.0', 'has the correct standard');
     });
   })
 
-  it('allocates the initial supply upon deployment', function () {
-    return Eniac.deployed().then(function (instance) {
+  it('allocates the initial supply upon deployment', () => {
+    return Eniac.deployed().then((instance) => {
       tokenInstance = instance;
       return tokenInstance.totalSupply();
-    }).then(function (totalSupply) {
+    }).then((totalSupply) => {
       assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
       return tokenInstance.balanceOf(accounts[0]);
-    }).then(function (adminBalance) {
+    }).then((adminBalance) => {
       assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supply to the admin account');
     });
   });
 
-  it('transfers token ownership', function () {
-    return Eniac.deployed().then(function (instance) {
+  it('transfers token ownership', () => {
+    return Eniac.deployed().then((instance) => {
       tokenInstance = instance;
       // Test `require` statement first by transferring something larger than the sender's balance
       return tokenInstance.transfer.call(accounts[1], 1000001);
-    }).then(assert.fail).catch(function (error) {
+    }).then(assert.fail).catch((error) => {
       assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
       return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
-    }).then(function (success) {
+    }).then((success) => {
       assert.equal(success, true, 'it returns true');
       return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
-    }).then(function (receipt) {
+    }).then((receipt) => {
       assert.equal(receipt.logs.length, 1, 'triggers one event');
       assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
       assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
       assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
       assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
       return tokenInstance.balanceOf(accounts[1]);
-    }).then(function (balance) {
+    }).then((balance) => {
       assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account');
       return tokenInstance.balanceOf(accounts[0]);
-    }).then(function (balance) {
+    }).then((balance) => {
       assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account');
     });
   });
 
-  it('approves tokens for delegated transfer', function () {
-    return Eniac.deployed().then(function (instance) {
+  it('approves tokens for delegated transfer', () => {
+    return Eniac.deployed().then((instance) => {
       tokenInstance = instance;
       return tokenInstance.approve.call(accounts[1], 100);
-    }).then(function (success) {
+    }).then((success) => {
       assert.equal(success, true, 'it returns true');
       return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
-    }).then(function (receipt) {
+    }).then((receipt) => {
       assert.equal(receipt.logs.length, 1, 'triggers one event');
       assert.equal(receipt.logs[0].event, 'Approval', 'should be the "Approval" event');
       assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized by');
       assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to');
       assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
       return tokenInstance.allowance(accounts[0], accounts[1]);
-    }).then(function (allowance) {
+    }).then((allowance) => {
       assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated trasnfer');
     });
   });
 
-  it('handles delegated token transfers', function () {
-    return Eniac.deployed().then(function (instance) {
+  it('handles delegated token transfers', () => {
+    return Eniac.deployed().then((instance) => {
       tokenInstance = instance;
       fromAccount = accounts[2];
       toAccount = accounts[3];
       spendingAccount = accounts[4];
       // Transfer some tokens to fromAccount
       return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
-    }).then(function (receipt) {
+    }).then((receipt) => {
       // Approve spendingAccount to spend 10 tokens form fromAccount
       return tokenInstance.approve(spendingAccount, 10, { from: fromAccount });
-    }).then(function (receipt) {
+    }).then((receipt) => {
       // Try transferring something larger than the sender's balance
       return tokenInstance.transferFrom(fromAccount, toAccount, 9999, { from: spendingAccount });
-    }).then(assert.fail).catch(function (error) {
+    }).then(assert.fail).catch((error) => {
       assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than balance');
       // Try transferring something larger than the approved amount
       return tokenInstance.transferFrom(fromAccount, toAccount, 20, { from: spendingAccount });
-    }).then(assert.fail).catch(function (error) {
+    }).then(assert.fail).catch((error) => {
       assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than approved amount');
       return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount });
-    }).then(function (success) {
+    }).then((success) => {
       assert.equal(success, true);
       return tokenInstance.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount });
-    }).then(function (receipt) {
+    }).then((receipt) => {
       assert.equal(receipt.logs.length, 1, 'triggers one event');
       assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
       assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transferred from');
       assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are transferred to');
       assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
       return tokenInstance.balanceOf(fromAccount);
-    }).then(function (balance) {
+    }).then((balance) => {
       assert.equal(balance.toNumber(), 90, 'deducts the amount from the sending account');
       return tokenInstance.balanceOf(toAccount);
-    }).then(function (balance) {
+    }).then((balance) => {
       assert.equal(balance.toNumber(), 10, 'adds the amount from the receiving account');
       return tokenInstance.allowance(fromAccount, spendingAccount);
-    }).then(function (allowance) {
+    }).then((allowance) => {
       assert.equal(allowance.toNumber(), 0, 'deducts the amount from the allowance');
     });
   });
--- test/eniacwallet.js
+++ test/eniacwallet.js
@@ -1,7 +1,7 @@
 var Eniac = artifacts.require('./Eniac.sol');
 var EniacWallet = artifacts.require('./EniacWallet.sol');
 
-contract('EniacWallet', function (accounts) {
+contract('EniacWallet', (accounts) => {
   var tokenInstance;
   var tokenSaleInstance;
   var admin = accounts[0];
@@ -10,75 +10,75 @@ contract('EniacWallet', function (accounts) {
   var tokensAvailable = 750000;
   var numberOfTokens;
 
-  it('initializes the contract with the correct values', function () {
-    return EniacWallet.deployed().then(function (instance) {
+  it('initializes the contract with the correct values', () => {
+    return EniacWallet.deployed().then((instance) => {
       tokenSaleInstance = instance;
       return tokenSaleInstance.address
-    }).then(function (address) {
+    }).then((address) => {
       assert.notEqual(address, 0x0, 'has contract address');
       return tokenSaleInstance.tokenContract();
-    }).then(function (address) {
+    }).then((address) => {
       assert.notEqual(address, 0x0, 'has token contract address');
       return tokenSaleInstance.tokenPrice();
-    }).then(function (price) {
+    }).then((price) => {
       assert.equal(price, tokenPrice, 'token price is correct');
     });
   });
 
-  it('facilitates token buying', function () {
-    return Eniac.deployed().then(function (instance) {
+  it('facilitates token buying', () => {
+    return Eniac.deployed().then((instance) => {
       // Grab token instance first
       tokenInstance = instance;
       return EniacWallet.deployed();
-    }).then(function (instance) {
+    }).then((instance) => {
       // Then grab token sale instance
       tokenSaleInstance = instance;
       // Provision 75% of all tokens to the token sale
       return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, { from: admin })
-    }).then(function (receipt) {
+    }).then((receipt) => {
       numberOfTokens = 10;
       return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice })
-    }).then(function (receipt) {
+    }).then((receipt) => {
       assert.equal(receipt.logs.length, 1, 'triggers one event');
       assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
       assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens');
       assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
       return tokenSaleInstance.tokensSold();
-    }).then(function (amount) {
+    }).then((amount) => {
       assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold');
       return tokenInstance.balanceOf(buyer);
-    }).then(function (balance) {
+    }).then((balance) => {
       assert.equal(balance.toNumber(), numberOfTokens);
       return tokenInstance.balanceOf(tokenSaleInstance.address);
-    }).then(function (balance) {
+    }).then((balance) => {
       assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
       // Try to buy tokens different from the ether value
       return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: 1 });
-    }).then(assert.fail).catch(function (error) {
+    }).then(assert.fail).catch((error) => {
       assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
       return tokenSaleInstance.buyTokens(800000, { from: buyer, value: numberOfTokens * tokenPrice })
-    }).then(assert.fail).catch(function (error) {
+    }).then(assert.fail).catch((error) => {
       assert(error.message.indexOf('revert') >= 0, 'cannot purchase more tokens than available');
     });
   });
 
-  it('ends token sale', function () {
-    return Eniac.deployed().then(function (instance) {
+  it('ends token sale', () => {
+    return Eniac.deployed().then((instance) => {
       // Grab token instance first
       tokenInstance = instance;
       return EniacWallet.deployed();
-    }).then(function (instance) {
+    }).then((instance) => {
       // Then grab token sale instance
       tokenSaleInstance = instance;
       // Try to end sale from account other than the admin
       return tokenSaleInstance.endSale({ from: buyer });
-    }).then(assert.fail).catch(function (error) {
+    }).then(assert.fail).catch((error) => {
       assert(error.message.indexOf('revert' >= 0, 'must be admin to end sale'));
       // End sale as admin
       return tokenSaleInstance.endSale({ from: admin });
-    }).then(function (receipt) {
+    }).then((receipt) => {
       return tokenInstance.balanceOf(admin);
-    }).then(async function (balance) {
+    }).then(async (balance) => {
       assert.equal(balance.toNumber(), 999990, 'returns all unsold dapp tokens to admin');
       return await web3.eth.getBalance(tokenSaleInstance.address)
     }).then((balance) => {
